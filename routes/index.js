d8615c2ab8d49c6b18b23f4512d16514eca288e8
AlfredoHC07 <alfredo09hernandez@gmail.com>
2021-11-26 09:52:23 -0400

    Se agrego mas contenido al curriculum y se cambio la foto. se agregaron las rutas para manejar la informacion.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const items = [];
+
 router.get('/', (req, res) =>{
-    res.render('index', {
-       title: 'Proyecto Curriculum 2.0'
+    res.render('curriculum', {
+       title: 'Proyecto Curriculum 2.0',
+       items: items
     })
 });
 
-router.get('/curriculum', (req, res, next) => {
-    res.render('curriculum', {
-        title: 'Curriculum 2.0'
-    })
+router.post('/contactos', (req, res, next) => {
+    const { newItem } = req.body;
+    items.push({
+        id: items.length + 1,
+        name: newItem
+    });
+    res.redirect('/contactos');
 });
 
 module.exports = router;
